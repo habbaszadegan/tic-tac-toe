6df41e80c7c20d8a83b2506f8700bdf9ad59a108
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2024-07-13 11:51:07 +0330

    Reworked logic. Working on winOrDrawCheck
    Previous functions were not reading into each other well.
    Current logic involves defining smaller functions related to separate
    sections of the game, and working them into an overall "game()" function
    Working on winOrDrawCheck() next

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function ticTacToe() {
 
-    const gameBoard = (function () {
+    // Game board and player functions
+    function gameBoard() {
         let board = [];
         let incrementor = 0;
         for (let i = 0; i < 3; i++) {
@@ -9,55 +10,74 @@ function ticTacToe() {
                 board[i][j] = incrementor++;
             }
         }
-        console.log(board);
-        return {board};
-    })();
 
-    const players = (function () {
-        const playerX = {
+        let newBoard = () => board;
+        return {newBoard};
+    };
+
+    function players() {
+        const X = {
             name: 'x',
             score: 0
         }
     
-        const playerO = {
+        const O = {
             name: 'o',
             score: 0
         }
+
+        let playerX = () => X;
+        let playerO = () => O;
+
         return {playerX, playerO}
-    });
+    };
 
-    function game() {
-        let currentGame = gameBoard();
-        let currentPlayers = players();
-
-    }
-
-    function takeTurns() {
+    // Game flow functions
+    function takeTurns(cGame) {
         let playerXInputRow = prompt(`X: Row?`);
         let playerXInputColumn = prompt(`Y: Column?`);
-        currentGame.board[playerXInputRow][playerXInputColumn] = 'X';
+        cGame.board[playerXInputRow][playerXInputColumn] = 'X';
 
         let playerOInputRow = prompt(`X: Row?`);
         let playerOInputColumn = prompt(`Y: Column?`);
-        currentGame.board[playerOInputRow][playerOInputColumn] = 'O';
+        cGame.board[playerOInputRow][playerOInputColumn] = 'O';
     };
 
-    function oneRound(turns, playrs) {
-        function winCheck () {
-            const winningConditions = [
-                [0, 1, 2], // First row
-                [3, 4, 5], // Second row
-                [6, 7, 8], // Third row
-                [0, 3, 6], // First column
-                [1, 4, 7], // Second column
-                [2, 5, 8], // Third column
-                [0, 4, 8], // First diagonal
-                [2, 4, 6], // Second diagonal
-            ];
+    function winOrDrawCheck (gBoard) {
+        const winningConditions = [
+            [0, 1, 2], // First row
+            [3, 4, 5], // Second row
+            [6, 7, 8], // Third row
+            [0, 3, 6], // First column
+            [1, 4, 7], // Second column
+            [2, 5, 8], // Third column
+            [0, 4, 8], // First diagonal
+            [2, 4, 6], // Second diagonal
+        ];
 
-            
-        }
+        let oCheck = [];
+        let xCheck = [];
+
+        // for (let i = 0; i < winningConditions.length; i++) {
+        //     let 
+        //     if ()
+        // }
     }
+
+    function game() { // This function is a mess and needs extensive reworking
+        let currentGame = gameBoard.board;
+        console.log(currentGame);
+        let currentPlayers = players();
+        takeTurns(currentGame);
+
+        function oneRound(turns, playrs, winDrwChk) {
+            // draw the gameboard
+                // choose your token
+                // take turns inputting your token on the board
+                // check win condition
+                // update score
+        }
+    }    
 };
 
 ticTacToe();
